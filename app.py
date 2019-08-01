9b8f34b420c9b1c51393ed688bcfc3411e0cd519
myname21-meet <myname21@meet.mit.edu>
2019-08-01 03:00:40 -0400

    buttons

--- app.py
+++ app.py
@@ -5,6 +5,27 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home2.html')
 
+@app.route('/dad')
+def dad():
+    return render_template('dad.html')
+
+@app.route('/mom')
+def mom():
+    return render_template('mom.html')
+
+@app.route('/dog')
+def dog():
+    return render_template('dog.html')
+
+@app.route('/brother')
+def brother():
+    return render_template('brother.html')
+
+@app.route('/sister')
+def sister():
+    return render_template('sister.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
